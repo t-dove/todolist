220c402ed268d5465734254845c2bdd49724b3f4
rodenpapa <roden.papa@yandex.ru>
2024-11-28 21:34:19 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-web';
+import Header from './components/header';
+import ListItem from './components/list';
+import Form from './components/form';
 
 export default function App() {
+  const deleteHandler = (key) => {
+    setListOfItems((list) => { return list.filter((listOfItems) => listOfItems.key != key) });
+  }
+  const addHandler = (text) => {
+    setListOfItems((list) => {
+      return [{ text: text, key: Math.random().toString(36).substring(7) }, ...list]
+    });
+  };
+  const [listOfItems, setListOfItems] = useState([
+    { text: "Купить слона", key: "1" },
+    { text: "Помыть крокодила", key: "2" },
+    { text: "Сделать дело", key: "3" },
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <Form addHandler={addHandler} />
+      <View>
+        <FlatList data={listOfItems}
+          renderItem={({ item }) => <ListItem el={item} deleteHandler={deleteHandler} />} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  header: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
